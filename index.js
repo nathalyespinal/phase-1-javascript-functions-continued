c7ac63766a70f9feec5987c720adc9bb00360556
NathalyEspinal <espinalnathaly@gmail.com>
2022-02-23 16:33:05 -0600

    commit tree f2ee75ef36d7a3f68d60427f9609c9cebe2058a4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+
+function saturdayFun(activity){
+    return `This Saturday, I want to ${'roller-skate!'}`
+   }
+saturdayFun("bathe my Dog!");
+
+
+function mondayWork(){
+    
+}
